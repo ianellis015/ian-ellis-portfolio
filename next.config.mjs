fcb2b7da0071afaf887e1f84a9109163f3c0a614
Ian Ellis <ianellis015@gmail.com>
2026-03-29 16:38:00 -0700

    fixing img quality issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    imageSizes: [256, 384, 640, 750, 828, 1080, 1200, 1920],
+    minimumCacheTTL: 60,
+  },
+};
 
 export default nextConfig;
